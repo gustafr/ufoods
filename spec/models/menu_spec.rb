bd55edd9ed753ad7bc7ea7e4609a8f13a432b09c
Amanda <29342045+Sajaas@users.noreply.github.com>
2017-10-10 14:18:29 +0200

    before adding id to table

--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe Menu, type: :model do
   describe 'DB table' do
     it { is_expected.to have_db_column :name }
   end
+
+  context `validations` do
+    it { is_expected.to belong_to(:restaurant) }
+  end
 end
